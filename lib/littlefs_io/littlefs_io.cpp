782a00a4de132c6d05d41ba26e31ccc3464c7995
Brandon Lee <bellee@chapman.edu>
2024-12-01 15:53:06 -0800

    Add support for readFile() function to return a String of the data's contents

--- lib/littlefs_io/littlefs_io.cpp
+++ lib/littlefs_io/littlefs_io.cpp
@@ -62,20 +62,24 @@ void removeDir(fs::FS &fs, const char *path) {
   }
 }
 
-void readFile(fs::FS &fs, const char *path) {
+String readFile(fs::FS &fs, const char *path) {
+  String data;
   Serial.printf("Reading file: %s\r\n", path);
 
   File file = fs.open(path);
   if (!file || file.isDirectory()) {
     Serial.println("- failed to open file for reading");
-    return;
+    return data;
   }
 
   Serial.println("- read from file:");
   while (file.available()) {
-    Serial.write(file.read());
+    int c = file.read();
+    Serial.write(c);
+    data.concat(c);
   }
   file.close();
+  return data;
 }
 
 void writeFile(fs::FS &fs, const char *path, const char *message) {
