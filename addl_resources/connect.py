adcb6db2aca9ad2eba94c0879eb7762d3e9e4a7b
Brandon Lee <bellee@chapman.edu>
2025-04-24 15:42:14 -0700

    Automatically connect to the ble_comms BLE server, read a characteristic, and output a CSV file

--- addl_resources/connect.py
+++ addl_resources/connect.py
@@ -5,9 +5,10 @@
 
 import platform
 import asyncio
+import time
 from bleak import BleakClient, BleakScanner
 
-drone_name = "EcoDrone"
+drone_name = "EcoDrone_Data"
 drone_transmit_uuid = "6e400003-b5a3-f393-e0a9-e50e24dcca9e" # WARNING: this UUID is hard linked to the drone! 
 macos_use_bdaddr = False # When true use Bluetooth address instead of UUID on macOS
 
@@ -21,30 +22,38 @@ async def main():
         drone_name, cb=dict(use_bdaddr=macos_use_bdaddr)
     )
     if device is None:
-        print("Could not find device with name '%s'", drone_name)
+        print(f"Could not find device with name {drone_name}")
         return
 
     print("Connecting to device...", end=" ", flush=True)
 
     async with BleakClient(device) as client:
-        print("Connected")
+        print("Connected!")
 
         # Search through all BLE services and all their characteristics
+        await client.get_services()
         for service in client.services:
+            print("Reading...", end="", flush=True)
             for char in service.characteristics:
-
                 if char.uuid == drone_transmit_uuid:
-                    if "read" in char.properties:
-                        try:
-                            # If this characteristic has values to read, read it and write it into "value"
-                            value = await client.read_gatt_char(char.uuid)
-                        except Exception as e:
-                            value = "Error: {e}"
+                    acq_data = False
+                    
+                    while not acq_data:
+                        if "read" in char.properties:
+                            try:
+                                # If this characteristic has values to read, read it and write it into "value"
+                                value = await client.read_gatt_char(char.uuid)
+                            except Exception as e:
+                                value = "Error: {e}"
 
-                    if not value:
-                        print("No value")
-                    else:
-                        print(value.decode("utf-8"))
+                        if not value:
+                            print(".", end="", flush=True)
+                        else:
+                            acq_data = True
+                            print()
+                            print(value.decode("utf-8"))
+                            with open("/Users/student/Documents/data.csv", "w") as text_file:
+                                text_file.write(f"{value.decode("utf-8")}")
 
         print("Disconnecting...", end=" ", flush=True)
     print("Disconnected")
